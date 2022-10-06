d798b300ac774e8a5c815afc13a735c59c1ac96a
Luciefifi <niyomutonilucie@gmail.com>
2022-10-06 21:02:35 +0200

    created classes.js file

---
+++ classes.js
@@ -0,0 +1,13 @@
+class person{
+    constructor(name)
+    {
+        this.name = name;
+        getName()
+         {
+            return this.name;
+        }
+    }
+}
+let stud = new person ("Lucie Lavender");
+let name = stud.getName();
+console.log(name);
