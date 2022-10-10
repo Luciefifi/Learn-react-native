800518b59ed401ffd0910aba010d1efb0121ea9c
Luciefifi <niyomutonilucie@gmail.com>
2022-10-10 04:48:58 +0200

    added computed propert

--- classes.js
+++ classes.js
@@ -17,3 +17,52 @@ let person = new Person('Jane Doe');
 console.log(person); // Jane Doe
 
 person.setName('jane smith');
+console.log(typeof person);
+//onsole.log( person instanceof person);
+let meeting = {
+    attendees: [],
+    add(attendee)
+    {
+        console.log(`${attendee} joined the meeting`)
+        this.attendees.push(attendee);
+        return this;
+    },
+};
+   function getlatest() 
+{
+    let count = this.attendees.length;
+    return count = 0 ? undefined : this.attendees[count-1];
+
+}
+meeting.add('Lucie');
+meeting.add('steve');
+meeting.add('Lavender');
+console.log(`the latest attendee is ${meeting.getlatest}.`);
+
+//computed property
+let propName = 'c';
+ const rank = {
+    a: 1,
+    b: 3,
+    [propName]: 2,
+};
+console.log(rank.c);
+
+let name = 'fullName';
+class Person1 {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  get [name]() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+let fname= new Person1('John', 'Doe');
+console.log(fname.fullName);   //John Doe
+
+
+
+
+
